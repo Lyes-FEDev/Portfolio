9cb3c766d5a31d89f0f3a9d44dfe1191d9b84ee6
Lyes <lyesmedjahed17@gmail.com>
2025-09-12 16:22:09 +0200

    update contact page and start with projects page

--- js/contact.js
+++ js/contact.js
@@ -20,19 +20,3 @@ icon.addEventListener('click', function(){
 })
 /* end Dark-theme*/
 
-/*PHONE FORM*/
-/*  const input = document.querySelector("#phone");
-  window.intlTelInput(input, {
-    loadUtils: () => import("https://cdn.jsdelivr.net/npm/intl-tel-input@25.10.6/build/js/utils.js"),
-  });*/
-
-const input = document.querySelector("#phone");
-window.intlTelInput(input, {
-  onlyCountries: ["al", "ad", "at", "by", "be", "ba", "bg", "hr", "cz", "dk",
-  "ee", "fo", "fi", "fr", "de", "gi", "gr", "va", "hu", "is", "ie", "it", "lv",
-  "li", "lt", "lu", "mk", "mt", "md", "mc", "me", "nl", "no", "pl", "pt", "ro",
-  "ru", "sm", "rs", "sk", "si", "es", "se", "ch", "ua", "gb"],
-  loadUtils: () => import("/intl-tel-input/js/utils.js?1757425287804") // for formatting/placeholders etc
-});
-
-/*END PHONE FORM */
